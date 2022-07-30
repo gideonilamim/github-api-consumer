a77579cc28a5b9fdab1249c095479b30fcad8fac
Gid.Lamim <gid.lamim@gmail.com>
2022-07-29 22:21:44 -0300

    change layout style

--- src/components/layout/styled.js
+++ src/components/layout/styled.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
 export const WrapperLayout = styled.section`
-  margin: 16px;
+  margin-top:90px;
+  margin-right: auto;
+  margin-left: auto;
+  display:flex;
+  width:1280px;
+  box-sizing: border-box;
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+font-size: 14px;
+line-height: 1.5;
+padding-right: 32px ;
+padding-left: 32px ;
+  
 `;
